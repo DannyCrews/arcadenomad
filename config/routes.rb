f48d8f7306c67a267cf1d2385a908efc6f749f47
dannycrews <dancrews@me.com>
2016-08-02 15:16:47 -0400

    Add games and about controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :about
+
+  match '/contact', to: 'about#new', via: 'get', as: 'contact_new'
+  match '/contact', to: 'about#create', via: 'post', as: 'contact'
+
+  resources :games
+
   resources :locations
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
