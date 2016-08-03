611df104dded8e45986a5e33d6899aeaab8f9fdc
dannycrews <dancrews@me.com>
2016-08-03 16:24:48 -0400

    Add Welcome controller and views. Set root route.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,20 @@
+class WelcomeController < ApplicationController
+
+  # Present the ArcadeNomad home page
+  def index
+
+    # Retrieve a random arcade
+    @location = Location.where(['id >= ?', rand(Location.count) + 1]).first
+
+    # Retrieve the most recently added arcades
+    @locations = Location.order('created_at desc').limit(5)
+
+    # Retrieve the most recently added barcades
+    @barcades = Location.latest_by_type('Barcade')
+
+    # Retrieve the most popular games according to appearance frequency
+    @popular_games = Game.popular.limit(5)
+
+  end
+
+end
